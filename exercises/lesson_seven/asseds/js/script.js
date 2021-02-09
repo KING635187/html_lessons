731d089b82215ce4159ea29a9fdd5b8a3f82dc8b
Shaxboz <ibragimbekovsahboz597@gmail.com>
2021-02-09 18:06:15 +0500

    Add lesson seven

---
+++ exercises/lesson_seven/asseds/js/script.js
@@ -0,0 +1,17 @@
+
+// Draw a Circle
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
+
+//var c = document.getElementById("youCanvas");
+//var ctx = c.getContext("2d");
+// Create gradient
+//var grd = ctx.createLinearGradient(0,0,200,0);
+//grd.addColorStop(0,"red");
+//grd.addColorStop(1,"white");
+// Fill with gradient
+//ctx.fillStyle = grd;
+//ctx.fillRect(10,10,150,80);
