a57fda42dde02085dc71be6a262165ce2a810b7a
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-06 19:13:57 +0500

    Add lesson 17

---
+++ teacher/lesson_17/assets/js/main.js
@@ -0,0 +1,10 @@
+document.getElementById("title").innerText = 5 + 6;
+document.write(45 * 2);
+// window.alert("Output with window alert");
+console.log("Output with console log");
+
+var lastName, lastname;
+lastName = "Doe";
+lastname = "Peterson";
+
+console.log(lastname);
