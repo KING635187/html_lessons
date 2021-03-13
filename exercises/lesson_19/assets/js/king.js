53c61ba86ca740f10953a91d94e6a4e4c978c791
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-03-13 19:23:36 +0500

    Add lesson 19

--- exercises/lesson_19/assets/js/king.js
+++ exercises/lesson_19/assets/js/king.js
@@ -1,22 +1,49 @@
-var a = 5;
-var b = 6;
-var c = 7;
-var s = (b + c + a)/2;
-var area = Math.sqrt(s*((s-a)*(s-b)*(s-c )));
-console.log(area)
+///////////////////////     №4
+
+// const a = 5, 
+//     b = 6, 
+//     c = 7, 
+//     s = (b + c + a) / 2,
+//     area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+
+// console.log(area)
 
 
+///////////////////////     №10
 
-function kopaytirish()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
-}
+// const numOne = document.getElementById("num-one"),
+//     numTwo = document.getElementById("num-two"),
+//     multiplyButton = document.getElementById("multiply"),
+//     divideButton = document.getElementById("divide");
 
-function bolish() 
-{ 
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-document.getElementById("result").innerHTML = num1 / num2;
-}
+// let result = document.getElementById("result");
+
+// multiplyButton.addEventListener('click', function() {
+//     result.innerHTML = numOne.value * numTwo.value;
+// });
+
+// divideButton.addEventListener('click', function() {
+//     result.innerHTML = numOne.value / numTwo.value;
+// });
+
+
+///////////////////////     №11
+
+// const input = prompt("Enter Celsius or Fahrenheit", "60C"),
+//     type = input[input.length - 1],
+//     value = input.substring(0, input.length - 1);
+
+// let output;
+
+// if (type.toLowerCase() == 'c') {
+//     output = value / 5 * 9 + 32 + "F";
+// } else if(type.toLowerCase() == 'f') {
+//     output = (value - 32) / 9 * 5 + "C";
+// } else {
+//     output = "Xato qiymat kiritdingiz!";
+// }
+
+// console.log(output);
+
+// console.log("ABCDEFGHIJKLMNOPQRS TUVWXYZ".length);
+// console.log("First line\nSecond line");
