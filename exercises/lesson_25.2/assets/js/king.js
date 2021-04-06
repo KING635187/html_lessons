2d941d5abe923c4d65bd575ba94eb3df456a0c4f
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-04-06 19:51:54 +0500

    Add calculator

--- exercises/lesson_25.2/assets/js/king.js
+++ exercises/lesson_25.2/assets/js/king.js
@@ -1,3 +1,32 @@
-funktion import(Number) {
-    document.getElementById("input").innerHTML = document.getElementById("input") + nummer;
+const clearButton = document.getElementById('clear'),
+    additionButtons = document.getElementsByClassName('addition'),
+    calculateButton = document.getElementById('calculate');
+var output = document.getElementById('output');
+
+[...additionButtons].map(function (additionButton) {
+    additionButton.addEventListener('click', function (event) {
+        const text = event.currentTarget.innerText;
+        if (new RegExp('[0-9]$').test(output.value)) {
+            show(text);
+        } else if (output.value === '' && new RegExp('[0-9]').test(text)) {
+            show(text);
+        } else if (new RegExp('[^0-9]$').test(output.value) && new RegExp('[0-9]').test(text)) {
+            show(text);
+        }
+    });
+});
+clearButton.addEventListener('click', clear);
+calculateButton.addEventListener('click', calculate);
+
+function show(text) {
+    output.value += text;
+}
+
+function calculate() {
+    const expressionString = output.value;
+    output.value = eval(expressionString);
+}
+
+function clear() {
+    output.value = '';
 }
