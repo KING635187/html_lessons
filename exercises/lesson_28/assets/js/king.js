2038c0275fe25b057b814f550904819d7912f25e
Shaxboz <ibragimbekovsahboz597@gmail.com>
2021-04-20 19:09:43 +0500

    Add lesson_28

--- exercises/lesson_28/assets/js/king.js
+++ exercises/lesson_28/assets/js/king.js
@@ -2,33 +2,25 @@
 var text = "";
 var i;
 for (i = 0; i < 10; i++) {
-  if (i === 3) { break; }
+  if (i === 3) break;
   text += "The number is " + i + "<br>";
 }
-// document.getElementById("demo").innerHTML = text;
+document.getElementById("demo").innerHTML = text;
 
 // The Continue Statement
 var text = "";
 var i;
 for (i = 0; i < 10; i++) {
-  if (i === 3) { continue; }
+  if (i === 3) continue;
   text += "The number is " + i + "<br>";
 }
-// document.getElementById("demo1").innerHTML = text;
+ document.getElementById("demo1").innerHTML = text;
 
 // JavaScript Labels
 var cars = ["BMW", "Volvo", "Saab", "Ford"];
 var text = "";
 
-list: {
-  text += cars[0] + "<br>"; 
-  text += cars[1] + "<br>"; 
-  break list;
-  text += cars[2] + "<br>"; 
-  text += cars[3] + "<br>"; 
-}
-
-// document.getElementById("demo2").innerHTML = text;
+ document.getElementById("demo2").innerHTML = text;
 
 // The typeof Operator
 document.getElementById("demo3").innerHTML = 
